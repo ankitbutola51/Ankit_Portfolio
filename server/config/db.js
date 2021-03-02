92dbaa9aaea0e4ee8c88412e4e2f365b8d7d309d
ankitbutola51 <ankitbutola51@gmail.com>
2021-03-02 13:48:25 +0530

    connected to mongo db  Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
 
-"URI" : "mongodb://localhost/business_clients"
+//"URI" : "mongodb://localhost/business_clients",
+"URI":"mongodb+srv://ankitbutola51:Today@911@clusterauth.2vscp.mongodb.net/business_clients?retryWrites=true&w=majority",
 }
